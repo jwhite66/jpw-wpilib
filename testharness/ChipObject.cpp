e45616d6b1a1357fc87f2173bc1de1f03f02fa04
Jeremy White <jwhite@codeweavers.com>
2012-12-02 14:03:55 -0600

    Stub implementation of tAI

--- testharness/ChipObject.cpp
+++ testharness/ChipObject.cpp
@@ -14,7 +14,55 @@ namespace nFPGA
     namespace nFRC_2012_1_6_4
     {
         tAccumulator* tAccumulator::create (unsigned char sys_index, tRioStatusCode *status) { UN_NULL }
-        tAI* tAI::create (unsigned char sys_index, tRioStatusCode *status) { UN_NULL }
+
+        class AIImpl : public tAI
+        {
+            public:
+                ~AIImpl(){ UN_VOID }
+                tSystemInterface* getSystemInterface()  { UN_NULL }
+                unsigned char getSystemIndex()  { UN_ZERO }
+                void writeConfig(tConfig value, tRioStatusCode *status)  { UN_VOID }
+                void writeConfig_ScanSize(unsigned char value, tRioStatusCode *status)  { UN_VOID }
+                void writeConfig_ConvertRate(unsigned int value, tRioStatusCode *status)  { UN_VOID }
+                tConfig readConfig(tRioStatusCode *status)
+                {
+                    UN_FPRINTF
+                    return m_config;
+                }
+                unsigned char readConfig_ScanSize(tRioStatusCode *status)  { UN_ZERO }
+                unsigned int readConfig_ConvertRate(tRioStatusCode *status)  { UN_ZERO }
+                void writeScanList(unsigned char bitfield_index, unsigned char value, tRioStatusCode *status)  { UN_VOID }
+                unsigned char readScanList(unsigned char bitfield_index, tRioStatusCode *status)  { UN_ZERO }
+                unsigned int readLoopTiming(tRioStatusCode *status)  { UN_ZERO }
+                void writeAverageBits(unsigned char bitfield_index, unsigned char value, tRioStatusCode *status)  { UN_VOID }
+                unsigned char readAverageBits(unsigned char bitfield_index, tRioStatusCode *status)  { UN_ZERO }
+                void writeOversampleBits(unsigned char bitfield_index, unsigned char value, tRioStatusCode *status)  { UN_VOID }
+                unsigned char readOversampleBits(unsigned char bitfield_index, tRioStatusCode *status)  { UN_ZERO }
+                signed int readOutput(tRioStatusCode *status)  { UN_ZERO }
+                void writeReadSelect(tReadSelect value, tRioStatusCode *status)  { UN_VOID }
+                void writeReadSelect_Channel(unsigned char value, tRioStatusCode *status)  { UN_VOID }
+                void writeReadSelect_Module(unsigned char value, tRioStatusCode *status)  { UN_VOID }
+                void writeReadSelect_Averaged(bool value, tRioStatusCode *status)  { UN_VOID }
+                tReadSelect readReadSelect(tRioStatusCode *status)
+                {
+                    UN_FPRINTF
+                    return m_select;
+                }
+                unsigned char readReadSelect_Channel(tRioStatusCode *status)  { UN_ZERO }
+                unsigned char readReadSelect_Module(tRioStatusCode *status)  { UN_ZERO }
+                bool readReadSelect_Averaged(tRioStatusCode *status)  { UN_ZERO }
+                void strobeLatchOutput(tRioStatusCode *status)  { UN_VOID}
+
+            protected:
+                tConfig m_config;
+                tReadSelect m_select;
+        };
+
+        tAI* tAI::create (unsigned char sys_index, tRioStatusCode *status)
+        {
+            fprintf(stderr, "%s(%d): %s Mostly UNIMPLEMENTED\n", __FILE__, __LINE__, __FUNCTION__);
+            return new AIImpl;
+        }
         tDIO* tDIO::create (unsigned char sys_index, tRioStatusCode *status) { UN_NULL }
         tInterrupt* tInterrupt::create (unsigned char sys_index, tRioStatusCode *status) { UN_NULL }
 
